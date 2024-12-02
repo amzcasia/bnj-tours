1950abe0fdd4721f6a1afb925745ae6e43f3ad67
Anton-Mark Casia <130978181+amzcasia@users.noreply.github.com>
2024-12-02 15:09:59 +0800

    fix for tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,4 @@
-import '@/styles/globals.css'
+import '@/styles/output.css';
 
 export default function RootLayout({
   children,
@@ -7,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="">{children}</body>
     </html>
   )
 }
